5dc46952c3fcc13a8cbe80695ccad0262a6d65b1
Jason Larke <jason.larke@curtin.edu.au>
2020-02-24 18:14:31 +0800

    Added more complex yang file to tests

--- tests/Monda.Yang.Tests/YangParserTests.cs
+++ tests/Monda.Yang.Tests/YangParserTests.cs
@@ -1,14 +1,21 @@
-﻿using System;
+﻿using System.IO;
 using Xunit;
 
 namespace Monda.Yang.Tests {
     public class YangParserTests {
         [Fact]
         public void ParseBasicYang() {
-            var yang = "module test { }";
-            var res = YangParser.KeywordParser.Then(YangParser.SeparatorParser).Parse(yang.AsSpan());
+            var yang = "module test {}";
             var statements = YangParser.Parse(yang);
             Assert.Equal(1, statements.Count);
+        }
+
+        [Fact]
+        public void ParseComplicatedYang() {
+            var yang = File.ReadAllText("./Examples/CISCO-PROCESS-MIB.yang");
+            var statements = YangParser.Parse(yang);
+            Assert.Equal(1, statements.Count);
+            Assert.True(statements[0].Children.Count > 0);
         }
     }
 }
